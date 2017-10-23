c7ea6ccc8beda517b2b450ccf7caf7971ec69ae8
ShawnYan <shawn2016@aliyun.com>
2017-10-23 14:05:33 +0800

    AIX

---
+++ unix/aix/AIX.md
@@ -0,0 +1,17 @@
+# IBM AIX
+
+[![AIX](../../svg/AIX-6.1-brightgreen.svg)]()
+
+
+- [Install RPM](rpm.sh)
+
+## REF
+
+- [AIX](https://www.ibm.com/power/operating-systems/aix)
+- [aix-jdk](https://www.ibm.com/developerworks/java/jdk/aix/service.html)
+- [Large Open Source Software Archive for AIX](http://www.bullfreeware.com/)
+
+### FTP
+
+- [ftp-ibm-aix](ftp://ftp.software.ibm.com/aix)
+- [ftp-RPMS](ftp://ftp.software.ibm.com/aix/freeSoftware/aixtoolbox/RPMS)
---
+++ unix/aix/rpm.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# gettext
+rpm -ivh gettext-0.19.7-2.aix6.1.ppc.rpm glib2-2.48.1-1.aix6.1.ppc.rpm libffi-3.0.13-1.aix5.3.ppc.rpm libiconv-1.14-22.aix6.1.ppc.rpm zlib-1.2.8-3.aix6.1.ppc.rpm ncurses-6.0p20160730-1.aix6.1.ppc.rpm libstdc++-6.3.0-1.aix6.1.ppc.rpm libgcc-6.3.0-1.aix6.1.ppc.rpm libunistring-0.9.6-2.aix6.1.ppc.rpm info-5.0-2.aix6.1.ppc.rpm bash-4.4-3.aix6.1.ppc.rpm libxml2-2.9.4-2.aix6.1.ppc.rpm xz-libs-5.2.2-2.aix6.1.ppc.rpm
+
+# gettext-devel
+rpm -ivh gettext-devel-0.19.7-2.aix6.1.ppc.rpm gettext-0.19.7-2.aix6.1.ppc.rpm glib2-2.48.1-1.aix6.1.ppc.rpm libffi-3.0.13-1.aix5.3.ppc.rpm libiconv-1.14-22.aix6.1.ppc.rpm zlib-1.2.8-3.aix6.1.ppc.rpm ncurses-6.0p20160730-1.aix6.1.ppc.rpm libstdc++-6.3.0-1.aix6.1.ppc.rpm libgcc-6.3.0-1.aix6.1.ppc.rpm libunistring-0.9.6-2.aix6.1.ppc.rpm info-5.0-2.aix6.1.ppc.rpm bash-4.4-3.aix6.1.ppc.rpm libxml2-2.9.4-2.aix6.1.ppc.rpm xz-libs-5.2.2-2.aix6.1.ppc.rpm
+
+# libiconv
+rpm -ivh libiconv-1.14-22.aix6.1.ppc.rpm gettext-0.19.7-2.aix6.1.ppc.rpm glib2-2.48.1-1.aix6.1.ppc.rpm libffi-3.0.13-1.aix5.3.ppc.rpm zlib-1.2.8-3.aix6.1.ppc.rpm ncurses-6.0p20160730-1.aix6.1.ppc.rpm libstdc++-6.3.0-1.aix6.1.ppc.rpm libgcc-6.3.0-1.aix6.1.ppc.rpm libunistring-0.9.6-2.aix6.1.ppc.rpm info-5.0-2.aix6.1.ppc.rpm bash-4.4-3.aix6.1.ppc.rpm libxml2-2.9.4-2.aix6.1.ppc.rpm xz-libs-5.2.2-2.aix6.1.ppc.rpm
+
+# pcre-devel
+rpm -ivh pcre-devel-8.39-1.aix6.1.ppc.rpm pcre-8.39-1.aix6.1.ppc.rpm bzip2-1.0.6-2.aix6.1.ppc.rpm readline-6.3-1.aix6.1.ppc.rpm info-5.0-2.aix6.1.ppc.rpm bash-4.4-3.aix6.1.ppc.rpm libiconv-1.14-22.aix6.1.ppc.rpm gettext-0.19.7-2.aix6.1.ppc.rpm glib2-2.48.1-1.aix6.1.ppc.rpm libffi-3.0.13-1.aix5.3.ppc.rpm zlib-1.2.8-3.aix6.1.ppc.rpm ncurses-6.0p20160730-1.aix6.1.ppc.rpm libstdc++-6.3.0-1.aix6.1.ppc.rpm libgcc-6.3.0-1.aix6.1.ppc.rpm libunistring-0.9.6-2.aix6.1.ppc.rpm libxml2-2.9.4-2.aix6.1.ppc.rpm xz-libs-5.2.2-2.aix6.1.ppc.rpm
+
+# zlib-devel
+rpm -ivh zlib-devel-1.2.8-3.aix6.1.ppc.rpm zlib-1.2.8-3.aix6.1.ppc.rpm
+
+# bash
+rpm -ivh bash-4.4-3.aix6.1.ppc.rpm libiconv-1.14-22.aix6.1.ppc.rpm gettext-0.19.7-2.aix6.1.ppc.rpm glib2-2.48.1-1.aix6.1.ppc.rpm libffi-3.0.13-1.aix5.3.ppc.rpm zlib-1.2.8-3.aix6.1.ppc.rpm ncurses-6.0p20160730-1.aix6.1.ppc.rpm libstdc++-6.3.0-1.aix6.1.ppc.rpm libgcc-6.3.0-1.aix6.1.ppc.rpm libunistring-0.9.6-2.aix6.1.ppc.rpm info-5.0-2.aix6.1.ppc.rpm libxml2-2.9.4-2.aix6.1.ppc.rpm xz-libs-5.2.2-2.aix6.1.ppc.rpm
+
+# gcc
+rpm -ivh gcc-4.8.5-5.aix6.1.ppc.rpm libgcc-4.8.5-5.aix6.1.ppc.rpm gcc-cpp-4.8.5-5.aix6.1.ppc.rpm gmp-6.1.1-1.aix6.1.ppc.rpm libstdc++-6.3.0-1.aix6.1.ppc.rpm libiconv-1.14-22.aix6.1.ppc.rpm gettext-0.19.7-2.aix6.1.ppc.rpm glib2-2.48.1-1.aix6.1.ppc.rpm libffi-3.0.13-1.aix5.3.ppc.rpm zlib-1.2.8-3.aix6.1.ppc.rpm ncurses-6.0p20160730-1.aix6.1.ppc.rpm libunistring-0.9.6-2.aix6.1.ppc.rpm info-5.0-2.aix6.1.ppc.rpm bash-4.4-3.aix6.1.ppc.rpm libxml2-2.9.4-2.aix6.1.ppc.rpm xz-libs-5.2.2-2.aix6.1.ppc.rpm libmpc-1.0.3-2.aix6.1.ppc.rpm mpfr-3.1.4-1.aix6.1.ppc.rpm
+
+# httpd
+rpm -ivh unixODBC-2.3.1-1.aix6.1.ppc.rpm
+rpm -ivh openssl-1.1.0e-1withsslv2.aix6.1.ppc.rpm
+rpm -ivh openldap-2.4.44-5.aix6.1.ppc.rpm db-4.8.24-4.aix6.1.ppc.rpm
+rpm -ivh apr-util-1.5.4-3.aix6.1.ppc.rpm apr-1.5.2-1.aix6.1.ppc.rpm expat-2.1.1-1.aix6.1.ppc.rpm
+rpm -ivh httpd-2.4.25-1.aix6.1.ppc.rpm
+
+## httpd-conf
+#cd /opt/freeware/etc/httpd/conf
+#vi httpd.conf
+
+## httpd-start
+#cd /opt/freeware/sbin
+#./apachectl -k start
+
+# redis
+rpm -ivh redis-2.6.16-1.aix6.1.ppc.rpm logrotate-3.8.3-1.aix6.1.ppc.rpm popt-1.16-2.aix6.1.ppc.rpm libiconv-1.14-22.aix6.1.ppc.rpm gettext-0.19.7-2.aix6.1.ppc.rpm glib2-2.48.1-1.aix6.1.ppc.rpm libffi-3.0.13-1.aix5.3.ppc.rpm zlib-1.2.8-3.aix6.1.ppc.rpm ncurses-6.0p20160730-1.aix6.1.ppc.rpm libstdc++-6.3.0-1.aix6.1.ppc.rpm libgcc-6.3.0-1.aix6.1.ppc.rpm libunistring-0.9.6-2.aix6.1.ppc.rpm info-5.0-2.aix6.1.ppc.rpm bash-4.4-3.aix6.1.ppc.rpm libxml2-2.9.4-2.aix6.1.ppc.rpm xz-libs-5.2.2-2.aix6.1.ppc.rpm coreutils-8.25-1.aix6.1.ppc.rpm gmp-6.1.1-1.aix6.1.ppc.rpm
+
